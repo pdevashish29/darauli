dcc6b5f8eacf4dbfa27562dfe880088cd10008d8
Parashar Devashish <devdewance@gmail.com>
2016-04-19 15:00:40 +0530

    second commit

--- src/main/java/com/pdp/controller/IndexController.java
+++ src/main/java/com/pdp/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping("/")
 	public String welcome(){
 		
 		return "welcome";
